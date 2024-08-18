52a3ea3007a67e5e6aeacdaa45fd9a0d054596a2
Julio Antonio <julioam766@gmail.com>
2024-08-18 23:26:05 +0700

    edit index and add script js

---
+++ script.js
@@ -0,0 +1,7 @@
+function toogleMene(){
+    const menu=document.querySelector(".menu-links")
+    const icon=document.querySelector(".hamburger-icon")
+    menu.classList.toogle("open");
+    icon.classList.toogle("open");
+}
+
